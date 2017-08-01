397a23d7f1a6b235a1ba17d994dd3aecd4382359
perro75 <stefan@mickels.fi>
2017-08-01 11:29:58 +0300

    Add files via upload

---
+++ EksML/Common.cs
@@ -0,0 +1,301 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: L708383
+ * Date: 11.5.2017
+ * Time: 16:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace EksML
+{
+	public enum fileType
+	{
+		ApplicationShema,
+		GmlBase,
+		GmlProfile
+	}
+	
+	public enum gmlType
+	{
+		SimpleType,
+		ComplexType
+	}
+	
+	public enum ElementType
+	{
+		ExtendsComplexType,
+		SameNameAsCompexType,
+		NoTypeDefinition
+	}
+	
+	
+	/// <summary>
+	/// Static class containing common functions needed for several purposes.
+	/// </summary>
+	public static class Common
+	{
+		// return string in double newlines
+		public static string  nlT1(string data, bool before, bool after)
+		{
+			var bf = before ? "\r\n" : "";
+			var af = after? "\r\n" : "";
+			data = "\t" + data.Replace("\r\n", "\r\n\t");
+			return  bf + data + af;
+		}
+
+		// return string in double newlines
+		public static string  nl(string data, bool before, bool after)
+		{
+			var bf = before ? "\r\n" : "";
+			var af = after? "\r\n" : "";
+			return  bf + data + af;
+		}
+		
+		private static string cleanComment(string original)
+		{
+			if (original == null)
+				return "";
+			
+			return System.Text.RegularExpressions.Regex.Replace(original.Trim().Replace("\r\n", " ").Replace("\r", " ").Replace("\n", " ").Replace("\t", " "), " {2,}", " ");
+		}
+		
+		// return string in double newlines
+		public static string  comment(string data)
+		{
+			//add comment if linebreak
+			data = "//" + cleanComment(data);
+			
+			return  nl(data, true, false);
+		}		
+		
+		// return string in double newlines
+		public static string  summary(string data)
+		{
+			//add comment if linebreak
+			data = "///" + cleanComment(data);
+			
+			return  nl("///<summary>", true, true) + data + nl("///</summary>", true, true);
+		}		
+		
+		// return string in double newlines
+		public static string  summaryT1(string data)
+		{
+			//add comment if linebreak
+			data = "\t///" + cleanComment(data);
+			
+			return  nl("\t///<summary>", true, true) + data + nl("\t///</summary>", true, true);
+		}
+		
+		public static string getNs(string name)
+		{
+			if (name == null)
+				return null;
+			
+			if (name.IndexOf(':') < 1)
+				return null;
+			
+			return name.Split(':')[0];
+			
+		}
+		
+		
+		public static string removeNs(string name)
+		{
+			
+			if (name == null)
+				return null;
+			
+					var item = name.Replace(' ', '_');
+					item = item.Replace(',', '_');
+					item = item.Replace('/', '_');
+					item = item.Replace('(', '_');
+					item = item.Replace(')', '_');
+					item = item.Replace('-', '_');
+					item = item.Replace('.', '_');
+					name = item.Replace('\'', '_');
+					
+					if (name.IndexOf(':') > 0)
+						name = name.Split(':')[1];
+			
+					
+			return convertBaseType(name);
+		}
+		
+		public static string convertBaseType(string type)
+		{
+			switch (type)
+			{
+				case "list_double":
+					return "double[]";
+					
+				case "boolean":
+					return "boolean";
+				
+				case "integer":
+					return "integer";
+			}
+			
+			return type;
+		}
+		
+		/// <summary>
+		/// Convert xs- types to C#
+		/// TODO: Add definitions of the missing types!
+		/// </summary>
+		/// <param name="type"></param>
+		/// <returns></returns>
+		private static string convertType(string type, string name)
+		{
+			switch (type)
+			{
+				case "xs:boolean":
+					return "bool";
+				
+				case "xs:integer":
+				case "xs:int": //XXX S-123 and S-201 uses xs.int
+					return "int";
+					
+				case "xs:string":
+				//case "xs:anyURI":
+					
+				case "S100:DataSetIdentificationType":
+				case "S100:DataSetStructureInformationType":
+					return "string";
+
+				/*<list itemType="double"/> is returned as list:double*/
+				case "list_double":
+					return "double[]";
+					
+				case "xs:gDay":
+				case "xs:gMonth":
+				case "xs:gYear":
+				case "xs:gMonthDay":
+				case "xs:gYearMonth":
+					return "string";
+				
+				case "xs:double":
+				case "xs:decimal":
+					return "double";
+				
+				case "xs:date":
+				case "xs:dateTime":
+					return "DateTime";
+				
+				case "xs:duration":
+				case "xs:time":					
+					return "TimeSpan";
+					
+				case "S100EXT:PointPropertyType":
+				case "S100EXT:CurvePropertyType":
+				case "S100EXT:SurfacePropertyType":
+				case "S100EXT:PointOrSurfacePropertyType":
+				case "S100EXT:PointCurveSurfacePropertyType":
+					
+				//case "gml:AbstractFeatureType":
+				//case "S100:AbstractFeatureType":
+				//case "S100:AbstractInformationType":
+				//case "gml:AbstractFeatureMemberType":
+					
+					return "object";
+				
+				case "gml:ReferenceType":
+					return lookupReference(name);
+					
+				default:
+					return cleanUp(type);
+			}
+		}
+		
+		//get type of asociated object based on role-name
+		//type in gml is always gml:referenceType
+		private static string lookupReference(string name)
+		{
+			switch(name)
+			{
+				case "appliesInLocation":
+						return "FeatureType";
+			
+				case "isApplicableTo":
+					return "ApplicabilityType";
+
+				case "theOrganisation":
+					return "Authority";
+				
+				case "providesInformation":
+					return "InformationTypeType";
+					
+				default:
+					return "object";
+			}
+		}
+		
+		/// <summary>
+		/// check names for reserved words and illegal naming
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		private static string checkReserved(string name)
+		{
+			string[] reserved = new string[3];
+			
+			reserved[0] = "private";
+			reserved[1] = "public";
+			reserved[2] = "fixed";
+			
+			foreach( string word in reserved)
+			{
+				if ( word.Equals(name))
+				{
+					return "_" + name;
+				}
+			}
+			
+			//starting with digit not allowed
+			if (Char.IsDigit(name[0]))
+		    {
+		    	return "_" + name;
+		    }
+			    
+			return name;
+		}
+		
+		//clean illegal characters
+		public static string cleanUp(string item)
+		{
+			switch (item)
+			{
+				case "-":
+					return "minus";
+				case "+":
+					return "plus";
+					
+				default:
+					item = item.Replace(' ', '_');
+					item = item.Replace(',', '_');
+					item = item.Replace('/', '_');
+					item = item.Replace('(', '_');
+					item = item.Replace(')', '_');
+					item = item.Replace('-', '_');
+					item = item.Replace('.', '_');
+					item = item.Replace('\'', '_');
+					
+					//namespaces
+					item = item.Replace("s100:", "S100.");
+					item = item.Replace("S100:", "S100.");
+					
+					item = item.Replace(':', '_');
+					
+					item = item.Replace("gml_", "gml.");
+					item = item.Replace("xs_double", "xs._double");
+					item = item.Replace("xs_", "xs.");
+					return checkReserved(item);
+			}
+						
+			
+		
+		}
+	
+	}
+}
---
+++ EksML/Types/SimpleType.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: L708383
+ * Date: 11.5.2017
+ * Time: 14:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Xml;
+
+namespace EksML
+{
+	/// <summary>
+	/// Class holds and prints Simple types read from XSD.
+	/// </summary>
+	public class SimpleType : BaseType
+	{
+
+		private string type;
+		
+		private string[] list_desc = new string[100];
+		private string[] list = new string[100];
+		
+		private int index = 0;
+		
+		private XmlNode simpleType;
+		
+		private string[] unionMembers;
+		
+		public bool isPartOfElement = false;
+		
+		
+		public SimpleType(string name, string type, string desc, XmlNode simpleType, string ns)
+		{
+			base.gType = gmlType.SimpleType;
+			
+			this.name = name;
+			this.type= Common.removeNs(type);
+			this.desc= desc;
+			this.ns = ns;
+			
+			this.simpleType = simpleType;
+		}
+		
+		public void addUnion(string[] memberTypes)
+		{
+			this.unionMembers = memberTypes;
+		}
+		
+			
+		public void addItem(string item, string desc)
+		{
+			this.list_desc[this.index] = desc;
+			this.list[this.index++] = item;
+		}
+		
+		
+		private string getItemDescription(int index)
+		{
+			return Common.summaryT1(this.list_desc[index]);
+		}
+		
+		public string printCsCode()
+		{
+			var cName = Common.cleanUp(this.name);
+			var cType= Common.removeNs(this.type);
+			
+			//if union found
+			if (this.unionMembers != null)
+			{
+				//Simple type as type
+				return this.getDescription() + "public struct " + cName +" { \r\n " + this.printUnionMembers() + "\r\n}";
+			}
+			
+			//if no enumeration
+			if (this.index == 0)
+			{
+				//Simple type as class
+				//return this.getDescription() + "public struct " + cName +" { " + Common.nlT1("public " + cType + " value { get; set; }", true, true) + "}";
+				
+				//Simple type as 
+				return this.getDescription() + this.printNewType(cName, cType);
+		
+			}
+			
+			//Enumeration as enum
+			return this.getDescription() + "public enum " + cName +" { " + this.printItems() + "\r\n}";
+		}
+			
+		private string printNewType(string csName, string csType)
+		{
+			var stringFormatted = "";
+			
+			if (csType.Equals("DateTime"))
+			{
+			stringFormatted = @"
+	
+	public string ToString(string format)
+	{
+		return this.VALUE.ToString(format);
+	}";
+			}
+			
+			var definition = @"
+public struct NewInt3 {
+
+	private Int32 VALUE;
+		
+	public static implicit operator Int32( NewInt3 value ) {
+		return value.VALUE;
+	}
+	
+	public static implicit operator NewInt3( Int32 value ) {
+		var _ReturnValue = new NewInt3();
+		_ReturnValue.VALUE = value;
+		return _ReturnValue;
+	}
+	
+	public override string ToString()
+	{
+		return this.VALUE.ToString();
+	}
+	" + stringFormatted + @"
+}";
+			
+			definition = definition.Replace("NewInt3", csName);
+			definition = definition.Replace("Int32", csType);
+			
+			return definition;
+		}
+
+		private string printItems()
+		{
+			string itemList = "";
+			
+			for (int i=0; i < this.index; i++)
+			{
+				var description = "[Description(\""+ this.list[i]+"\")]";
+				//print enumeration items
+				itemList += this.getItemDescription(i) + "\t" + description +"\r\n\t" + Common.cleanUp(this.list[i]) + "=" + (i+1) + ",";
+			}
+		
+			return itemList.Trim(',');
+		}
+		
+		private string printUnionMembers()
+		{
+			var union = "";
+			
+			foreach (var mem in this.unionMembers)
+			{
+				var cType = Common.cleanUp(mem);
+				var cName = Common.removeNs(mem);
+				cType = Common.convertBaseType(cType);
+				
+				union += "\tpublic " + cType + " " + "_" + cName + ";\r\n";
+			}
+		
+			return union;
+		}
+		
+		public string printDebugLine(string printRequester)
+		{
+			return (printRequester + ";SimpleType;" + this.ns + ";" +  this.type + ";"+ this.name);
+		}
+		
+	}
+}
